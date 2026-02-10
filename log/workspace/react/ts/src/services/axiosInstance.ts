723d45de30170b746f008aa755c9df0666970721
4BFC <4bee.code@gmail.com>
2026-02-10 23:46:45 +0900

    chore: reqres API 등록 및 CORS로 인한 vite-proxy 설정

--- log/workspace/react/ts/src/services/axiosInstance.ts
+++ log/workspace/react/ts/src/services/axiosInstance.ts
@@ -1,11 +1,12 @@
 import axios from "axios";
 
-const API = 'https://reqres.in/api'
+const API = '/api'
 
 export const axiosInstance = axios.create({
     baseURL: API,
     timeout: 10000,
     headers: {
         'Content-Type': 'application/json',
+        'x-api-key': import.meta.env.VITE_REQRES_API_KEY,
     }
 })
--- log/workspace/react/ts/vite.config.ts
+++ log/workspace/react/ts/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import Pages from 'vite-plugin-pages'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,4 +11,13 @@ export default defineConfig({
       dirs: 'src/pages'
     })
   ],
+  envDir: path.resolve(__dirname, '../../../'),
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://reqres.in',
+        changeOrigin: true,
+      }
+    }
+  }
 })
