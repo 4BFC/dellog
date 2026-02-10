8c0acf65fcb8ff4f3f938399b510440e45082a56
4BFC <4bee.code@gmail.com>
2026-02-10 23:45:37 +0900

    feat: user를 조회하고 생성할 수 있는 훅 구현

---
+++ log/workspace/react/ts/src/hooks/useUsers.ts
@@ -0,0 +1,38 @@
+// TODO: QueryKey를 관리 할 수 있는 디렉토리 필요
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import { getUsers, getUser, createUser } from "../services/users.axios";
+
+export const userKeys = {
+    all: ['users'] as const,
+    lists: () => [...userKeys.all, 'list'] as const,
+    list: (page: number) => [...userKeys.lists(), page] as const,
+    details: () => [...userKeys.all, 'detail'] as const,
+    detail: (id: number) => [...userKeys.details(), id] as const,
+}
+
+export function useUsers(page = 1){
+    return useQuery({
+        queryKey: userKeys.list(page),
+        queryFn: () => getUsers(page),
+    })
+}
+
+export function useUser(id: number){
+    return useQuery({
+        queryKey: userKeys.detail(id),
+        queryFn: () => getUser(id),
+        enabled: !!id,
+    })
+}
+
+
+export function useCreateUser(){
+    const QueryClient = useQueryClient()
+
+    return useMutation({
+        mutationFn: createUser,
+        onSuccess: () => {
+            QueryClient.invalidateQueries({queryKey: userKeys.lists()})
+        }
+    })
+}
